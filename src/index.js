73e35f4c805104683ea595b182b9de9f50c639f8
Daeyeon Kim <danielkim@16KimD.local>
2020-05-26 17:32:29 +0900

    create GlobalStyle and inject it to App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
+import './styles/global.css';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+		margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+		cursor: default;
+		color: rgba(255,255,255,1);
+		margin-left: 10px;
+		margin-bottom: 10px;
+		font-size: 25px;
+  }
+`;
 
 ReactDOM.render(
 	<React.StrictMode>
+		<GlobalStyle />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root'),
