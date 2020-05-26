f3a986c24e0dd0cd2dbfd9b8d7da29bee0cb2dbc
Daeyeon Kim <danielkim@16KimD.local>
2020-05-26 17:33:14 +0900

    Inject ThemeProvider, Containers, and PageProvider to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import PageProvider from './components/providers/PageProvider';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import { AppContainer, CardContainer } from './styles/StyleContainer';
+import IntroPage from './pages/IntroPage';
+import IntroLoadingPage from './pages/IntroLoadingPage';
+import QuestionsPage from './pages/QuestionsPage';
+import VerificationPage from './pages/VerificationPage';
+import ResultLoadingPage from './pages/ResultLoadingPage';
+import ResultPage from './pages/ResultPage';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<AppContainer className='App'>
+				<CardContainer>
+					<PageProvider>
+						<IntroPage />
+						<IntroLoadingPage />
+						<QuestionsPage />
+						<VerificationPage />
+						<ResultLoadingPage />
+						<ResultPage />
+					</PageProvider>
+				</CardContainer>
+			</AppContainer>
+		</ThemeProvider>
+	);
 }
 
 export default App;
