65955d1f255527885f2016a9fd6ff9c2583a2f9b
Daeyeon Kim <danielkim@16KimD.local>
2020-05-29 18:20:07 +0900

    Update Choice to be compatible with real data

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -2,28 +2,33 @@ import React, { useState, useCallback, useContext } from 'react';
 import PageContext from '../contexts/Page.context';
 import styled from 'styled-components';
 
-const Choice = ({ currentPage, choice }) => {
+const Choice = ({ choice, index }) => {
 	// sets the color of choice when it's clicked.
 	const [choiceColor, setChoiceColor] = useState('rgba(29, 188, 165, 1)');
 	const { setPageCount, addUserChoice } = useContext(PageContext);
 	const onChoiceClick = useCallback(() => {
 		setChoiceColor('rgba(249, 180, 56, 1)');
-		addUserChoice(choice);
+		addUserChoice(index);
 		setPageCount();
-	}, [choice, addUserChoice, setPageCount]);
+	}, [index, addUserChoice, setPageCount]);
 	return (
 		<ChoiceWrapper choiceColor={choiceColor} onClick={onChoiceClick}>
-			{currentPage}_{choice}
+			{choice}
 		</ChoiceWrapper>
 	);
 };
 
 const ChoiceWrapper = styled.div`
 	margin-bottom: 1.5em;
-	border: 1px solid black;
+	border: 1px solid #148776;
 	border-radius: 15px;
-	padding: 0.8em 4.5em;
+	border-top-width: medium;
+	border-left-width: medium;
+	// padding: 0.8em 4.5em;
 	background-color: ${(props) => props.choiceColor || 'rgba(29, 188, 165, 1)'};
+	width: 100%;
+	padding: 10px 20px;
+	font-size: 18px;
 `;
 
 export default Choice;
