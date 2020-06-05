d8df4f11ed044bdd91385bb169a5254d05a101b2
Daeyeon Kim <danielkim@16KimD.local>
2020-06-05 13:20:49 +0900

    Relocate Sentry init in index.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PageProvider from './components/providers/PageProvider';
 import { AppContainer, CardContainer } from './styles/StyleContainer';
 import PageController from './pages/PageController';
-import * as Sentry from '@sentry/browser';
-
-Sentry.init({dsn: "https://1ab2dd6d33c842b0a85ca08664f059fe@o403093.ingest.sentry.io/5265231"});
 
 function App() {
 	return (
